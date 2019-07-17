c9993f37305965930a019dcd21b4c8c043aa87d4
Ernesto Alejandro Nappi <eanappi@gmail.com>
2019-07-17 12:20:29 -0300

    API's for app

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
-fetch('https://randomuser.me/api/1.2/?nat=es&gender=female&inc=gender,name,nat')
+fetch('https://randomuser.me/api/1.2/?nat=es&gender=female&results=2&inc=gender,name,nat')
   .then(response => response.json())
   .then(data => console.log(data))
+
+
+  fetch('https://significadodenombres24.com/nombres-con-a/feed/')
+  .then(response => response.json())
+  .then(data => console.log(data))
+
+  
