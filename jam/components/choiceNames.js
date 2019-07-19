813c4e50353de15317954c97bd0941e757a8ff32
Ernesto Alejandro Nappi <eanappi@gmail.com>
2019-07-19 11:49:52 -0300

    Only get and show one name

--- jam/components/choiceNames.js
+++ jam/components/choiceNames.js
@@ -1,7 +1,7 @@
 const elementWriteName = document.querySelector('#home h1 span')
 
 export default (gender = 'female') => {
-  fetch(`https://randomuser.me/api/1.2/?nat=es&gender=${gender}&results=2&inc=gender,name,nat`)
+  fetch(`https://randomuser.me/api/1.2/?nat=es&gender=${gender}&results=1&inc=gender,name,nat`)
   .then(response => {
     if (response.ok) {
       return response.json().then(data => {
